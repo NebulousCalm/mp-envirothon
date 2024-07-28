b8ac80d1f113459dc1e9f498d56eec20b036e20c
zjric <richmanzj@gmail.com>
2024-07-28 15:42:38 -0400

    please work im sorry

--- app/app/footer.js
+++ app/app/footer.js
@@ -9,7 +9,7 @@ import { faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons'
 export default function Footer(){
   return(
     <div className='footer-container'>
-      <Link href="#"><FontAwesomeIcon icon={faEnvelope} className="footer-icons"/></Link>
+      <Link href="mailto:zjrichman@outlook.com"><FontAwesomeIcon icon={faEnvelope} className="footer-icons"/></Link>
       <Link href="https://www.instagram.com/mp.envirothon/"><FontAwesomeIcon icon={faInstagram} className="footer-icons"/></Link>
       <Link href="https://www.github.com/NebulousCalm/mp-envirothon"><FontAwesomeIcon icon={faGithub} className="footer-icons"/></Link>
       <Link href="https://www.remind.com/join/@mpenvithon"><Image src="/remind-logo.png" width={30} height={30} /></Link>
